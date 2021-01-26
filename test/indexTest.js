da94cc92960b36cf0d6942392ca13bba067b3c02
zhangbin_yq <zhangbin_yq@kedacom.com>
2021-01-26 19:59:07 +0800

    深拷贝

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,11 @@
-import * as chai from "chai";
-import * as sinon from "sinon";
-import * as sinonChai from "sinon-chai";
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 const assert = chai.assert;
 
-const deepClone =require('../src/deepClone');
+const deepClone = require('../src/deepClone');
 describe('deepClone', () => {
     it('是一个函数', () => {
         assert.isFunction(deepClone);
@@ -15,9 +15,9 @@ describe('deepClone', () => {
         const n2 = deepClone(n);
         assert(n === n2);
     });
-    describe('对象', ()=> {
+    describe('对象', () => {
         it('能够复制普通对象', () => {
-            const a = {name: 'binbin', child: { name: 'xiaoxiao'}};
+            const a = {name: 'binbin', child: {name: 'xiaoxiao'}};
             const a2 = deepClone(a);
             assert(a !== a2);
             assert(a.name === a2.name);
@@ -25,18 +25,18 @@ describe('deepClone', () => {
             assert(a.child.name === a2.child.name);
         });
         it('能够复制数组对象', () => {
-            const a = [[11,12,13],[21,22,23], [31,32]];
+            const a = [[11, 12, 13], [21, 22, 23], [31, 32]];
             const a2 = deepClone(a);
             assert(a !== a2);
             assert(a[0] !== a2[0]);
             assert(a[1] !== a2[1]);
             assert(a[2] !== a2[2]);
-            assert.deepEqual(a, a2)
+            assert.deepEqual(a, a2);
         });
-        it('能够复制函数', ()=> {
-            const a = function (){
-                return 1
-            }
+        it('能够复制函数', () => {
+            const a = function () {
+                return 1;
+            };
             a.xxx = {yyy: {zzz: 1}};
             const a2 = deepClone(a);
             assert(a !== a2);
@@ -44,6 +44,14 @@ describe('deepClone', () => {
             assert(a.xxx !== a2.xxx);
             assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
             assert(a() === a2());
-        })
-    })
-})
+        });
+        it('能够复制环状对象', () => {
+            const a = {xxx: 'xxx'};
+            a.self = a;
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.xxx === a2.xxx);
+            assert(a.self !== a2.self);
+        });
+    });
+});
